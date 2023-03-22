98d3d691b0f32ea633646af5be2e59f58d55830a
Ahamed Careem <68745704+amcareem@users.noreply.github.com>
2023-03-22 22:21:50 +0530

    Create IntrotoPython.py

---
+++ IntrotoPython.py
@@ -0,0 +1,8 @@
+//Very basic calculator
+//start by typing python in the Cloud Shell console
+first_number = int(input('Type the first number: ')) ;\
+second_number = int(input('Type the second number: ')) ;\
+
+//The characters ;\, at the end of the first two statements signal that there are many lines of code. Using these characters lets you input all of the code, line by line.
+
+print("The sum is: ", first_number + second_number)
